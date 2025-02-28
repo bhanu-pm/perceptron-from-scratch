200e5eeb1eb8d6243bd6a6ed65ac500bb945016b
bhanu-pm <bhanuprakashm1108@gmail.com>
2025-02-27 22:50:46 -0700

    Perceptron code

--- perceptron.py
+++ perceptron.py
@@ -10,9 +10,16 @@ def perceptron(*args: float, **kwargs: float):
 
 	inputs2 = np.array(remaining_inps)
 	inputs = np.concatenate((inputs1, inputs2))
+	# print(inputs.shape)
 
-	print(inputs)
+	weights = np.ones(inputs.shape, dtype=float)
+	# print(weights.shape)
+	# print(weights)
+	weights_0 = 0
+	y = (weights @ inputs) + weights_0
 
+	return y
 
 perceptron(1, 2, x3=3, x4=4)
- 
+
+
