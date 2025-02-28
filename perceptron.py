9161b20ce6514f8354ace46f5113266d4c24fb61
bhanu-pm <bhanuprakashm1108@gmail.com>
2025-02-27 22:10:23 -0700

    Reading inputs

--- perceptron.py
+++ perceptron.py
@@ -1,1 +1,18 @@
 # Perceptron code 
+import numpy as np
+
+
+def perceptron(*args: float, **kwargs: float):
+	inputs1 = np.array(args)
+	remaining_inps = []
+	for i, j in kwargs.items():
+		remaining_inps.append(j)
+
+	inputs2 = np.array(remaining_inps)
+	inputs = np.concatenate((inputs1, inputs2))
+
+	print(inputs)
+
+
+perceptron(1, 2, x3=3, x4=4)
+ 
