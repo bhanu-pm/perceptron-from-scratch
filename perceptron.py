b06c6ca66a5933c52c019cf5d21c70e17f40e829
bhanu-pm <bhanuprakashm1108@gmail.com>
2025-02-28 19:44:38 -0700

    completed perceptron and training loop

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,5 @@
 # Perceptron code 
+import pandas as pd
 import numpy as np
 
 
@@ -37,13 +38,43 @@ class TrainingNeuron:
 		self.weights[0] = self.weights[0] + (self.learning_rate * loss_val)
 		self.weights[1:] = self.weights[1:] + (self.learning_rate * loss_val * inputs)
 
+	def backward(self, inputs, y, y_hat):
+		loss_val = self.loss(y, y_hat)
+		self.update_rule(loss_val, inputs)
+		return loss_val
+
 
 if __name__ == "__main__":
-	num_inputs = int(input("Enter the number of inputs: integer: "))
-	str_inputs = input("Enter the inputs, delimited by a comma: ").split(',')
+	# num_inputs = int(input("Enter the number of inputs: integer: "))
+	# str_inputs = input("Enter the inputs, delimited by a comma: ").split(',')
 
-	inputs = [int(i) for i in str_inputs]
+	# inputs = [int(i) for i in str_inputs]
 
-	training = TrainingNeuron(num_inputs, 1e-3)
-	y_hat = training.forward(inputs, "sigmoid")
-	print(y_hat)
+	### TRAINING LOOP
+	ds = pd.read_csv("ds.csv")
+	# print(ds.head(10))
+	dataset = ds.to_numpy()
+
+	training = TrainingNeuron(num_inputs, 1e-2)
+	# y_hat = training.forward(inputs, "relu")
+	# print(y_hat)
+
+
+	# Train test split
+	train_ds = dataset[:80, :]
+	test_ds = dataset[80:, :]
+
+	num_inputs = 3
+	activation = "relu"
+	epochs = 10
+	for epoch in epochs:
+		for i in train_ds:
+			# split into X and Y
+			x, y = i[:-1], i[-1]
+
+			# forward pass
+			y_hat = training.forward(x, activation)
+
+			# calculate loss
+			# backward pass to update weights
+			loss_val = training.backward(x, y, y_hat)
